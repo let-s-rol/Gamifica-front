e6592d33f94198367139af514a207f9572da2a75
IagoPL <iagoprietolamas@gmail.com>
2023-02-15 20:20:54 +0100

    separar validadores

--- Frontend/src/app/register/register.component.ts
+++ Frontend/src/app/register/register.component.ts
@@ -7,10 +7,9 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   user: FormGroup;
 
-  isTeacher: boolean = true;
+  isTeacher: boolean = false;
 
   constructor() {
     this.user = new FormGroup({
@@ -29,17 +28,21 @@ export class RegisterComponent implements OnInit {
         Validators.required,
         Validators.minLength(3),
       ]), //TODO hacer que se comprueben que las contraseñas son iguales
-      date: new FormControl('', Validators.required), //TODO: mirar de hacer que comprueba una fecha razonable
     });
-    
+
+    if (this.isTeacher) {
+      this.user.addControl('school', new FormControl(' Validators.required'));
+    } else {
+      this.user.addControl('date', new FormControl(' Validators.required')); //TODO: mirar de hacer que comprueba una fecha razonable
+    }
   }
   send(): any {
     //TODO: Usar esta funcion para mandar los datos al back
     console.log(this.user.value);
   }
   changeRol(): void {
-    console.log("click");
-    
+    console.log('click');
+
     this.isTeacher = !this.isTeacher;
   }
   ngOnInit(): void {}
