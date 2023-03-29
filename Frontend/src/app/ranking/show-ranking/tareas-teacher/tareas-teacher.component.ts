6863cb76349aa7508022eb643bfa56f4ce226224
IagoPL <iagoprietolamas@gmail.com>
2023-03-29 16:35:42 +0200

    crear tarea

--- Frontend/src/app/ranking/show-ranking/tareas-teacher/tareas-teacher.component.ts
+++ Frontend/src/app/ranking/show-ranking/tareas-teacher/tareas-teacher.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tareas-teacher',
   templateUrl: './tareas-teacher.component.html',
-  styleUrls: ['./tareas-teacher.component.css']
+  styleUrls: ['./tareas-teacher.component.css'],
 })
 export class TareasTeacherComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  user!: FormGroup;
+  constructor() {
+    this.user = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      task: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    });
   }
 
+  send() {
+  // TODO Hacer enlace y que guarde el enunciado
+
+  }
+
+  ngOnInit(): void {}
 }
