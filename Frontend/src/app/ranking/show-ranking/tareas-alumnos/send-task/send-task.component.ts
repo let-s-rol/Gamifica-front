0a13f68cafaa138581851703bf820142695a012d
IagoPL <iagoprietolamas@gmail.com>
2023-04-28 17:11:14 +0200

    cambio

--- Frontend/src/app/ranking/show-ranking/tareas-alumnos/send-task/send-task.component.ts
+++ Frontend/src/app/ranking/show-ranking/tareas-alumnos/send-task/send-task.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { UsersService } from 'src/app/services/users/users.service';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-send-task',
@@ -39,22 +40,35 @@ export class SendTaskComponent implements OnInit {
     };
   }
 
-
   uploadPdf(pdf: string, id_task: number) {
+    const token = localStorage.getItem('access_token');
+    const headers = { Authorization: `Bearer ${token}` };
+
     const url = 'http://127.0.0.1:8000/api/pdf/upload'; // Reemplaza esto con la URL correcta de tu backend
     const data = {
       pdf,
-      id_task
+      id_task,
     };
-    return this.http.post(url, data);
-  }
+    console.log(data, headers);
   
+    return this.http.post(url, data, { headers }).pipe(
+      catchError((error) => {
+        console.error(error);
+        return throwError(error);
+      })
+    );
+
+    
+
+
+      
+  }
 
   sendToBackend(base64: string, idUser: number) {
     const url = 'http://127.0.0.1:8000/api/pdf/upload';
 
-    console.log(base64 + " " + idUser);
-    
+    console.log(base64 + ' ' + idUser);
+
     const data = { pdf: base64, id_task: idUser };
     this.http.post(url, data).subscribe(
       (response: any) => {
